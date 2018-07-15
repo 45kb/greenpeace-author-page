d8ec517a0f75e149010336d447f00d6956196dcc
45kb <filippo.oretti@gmail.com>
2018-07-15 17:22:09 +0200

    using vars for css media query , linted js file

--- src/js/back-to-top-button.js
+++ src/js/back-to-top-button.js
@@ -1,24 +1,31 @@
 // Scroll to top button if present in page
-$(document).ready(function() {
-if ($('.back-to-top').length) {
-    var scrollTrigger = $(window).height(),
-        backToTop = function () {
-            var scrollTop = $(window).scrollTop();
-            if (scrollTop > scrollTrigger) {
-                $('.back-to-top').addClass('show');
-            } else {
-                $('.back-to-top').removeClass('show');
-            }
-        };
+$(document).ready(function () {
+  var backToTopButtonScrollTrigger = $(window).height();
+
+  if ($('.back-to-top').length) {
+
+    var backToTop = function () {
+      var scrollTop = $(window).scrollTop();
+      if (scrollTop > backToTopButtonScrollTrigger) {
+        $('.back-to-top').addClass('show');
+      } else {
+        $('.back-to-top').removeClass('show');
+      }
+    };
+
     backToTop();
+
     $(window).on('scroll', function () {
-        backToTop();
+      backToTop();
     });
+
     $('.back-to-top').on('click', function (e) {
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 700);
+
+      e.preventDefault();
+
+      $('html,body').animate({
+        scrollTop: 0,
+      }, 700);
     });
-}
+  }
 });
